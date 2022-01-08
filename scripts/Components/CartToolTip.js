88432b69acf490dbc5b1f0b2f8151396397531fb
Vadym Dashchenko <vadym.dashchenko@yaro.tech.com>
2022-01-08 21:27:11 +0200

    add babel

---
+++ scripts/Components/CartToolTip.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CartToolTip = () => {
+    return (
+      <div>
+         <h1>Hello i'm react element bitch</h1>
+      </div>
+    );
+}
+
+export default CartToolTip;
